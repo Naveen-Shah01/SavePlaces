56448be98aacd7e6193c0618603ef3ba140a3490
Naveen Shah <naveenshah_me20a10_05@dtu.ac.in>
2023-01-24 18:58:21 +0530

    some changes

---
+++ app/src/main/java/com/example/saveplaces/AddYourPlace.kt
@@ -0,0 +1,11 @@
+package com.example.saveplaces
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddYourPlace : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_your_place)
+    }
+}
--- app/src/main/java/com/example/saveplaces/MainActivity.kt
+++ app/src/main/java/com/example/saveplaces/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.saveplaces
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.saveplaces.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+
+        binding?.fabAddHappyPlace?.setOnClickListener {
+            val intent = Intent(this, AddYourPlace::class.java)
+            startActivity(intent)
+        }
     }
 }
